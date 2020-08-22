55f1e20f4471ad923e3bf143aacb023a38f74df3
gianmillare <gianmillaregsm@gmail.com>
2020-08-21 22:14:22 -0700

    pushing the completed project, the project was completed via CS50 IDE

---
+++ Homepage/index.js
@@ -0,0 +1,11 @@
+function correct(checkbox) {
+    if (checkbox.checked) {
+        alert('You are Correct!');
+    }
+}
+
+function incorrect(checkbox) {
+    if (checkbox.checked) {
+        alert('Oops try again!');
+    }
+}
